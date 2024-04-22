f1f9d3bc3c6491e8d21dbf47e372731ae2400215
detrothe <betonkanu@web.de>
2024-04-22 11:02:16 +0200

    22.4.2024

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,9 @@ import './styles/contextMenu.css';
 import './pages/haupt';
 
 const isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1;
+console.log("isAndroid =",isAndroid)
 
-if ( isAndroid) {
+if (isAndroid) {
     // window.addEventListener('load', function() {
     //     window.history.pushState({}, '')
     //   })
@@ -16,38 +17,40 @@ if ( isAndroid) {
     //     window.history.pushState({}, '')
     //   })
 
-      window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
         window.history.pushState({ noBackExitsApp: true }, '')
-      })
+    })
 
-      window.addEventListener('popstate', function(event) {
+    window.addEventListener('popstate', function (event) {
         if (event.state && event.state.noBackExitsApp) {
-          window.history.pushState({ noBackExitsApp: true }, '')
+            window.history.pushState({ noBackExitsApp: true }, '')
         }
-      })
-}
+    })
 
-// Damit man nicht versehentlich das Browserfenster löscht und Eingaben verliert
-
-window.addEventListener('beforeunload', function(event) {
-    event.preventDefault();
-    // Google Chrome requires returnValue to be set.
-    event.returnValue = '';
-    console.log("beforeunload", event)
-  });
-
-  if ('launchQueue' in window) {
-    console.log('File Handling API is supported!');
-
-    //@ts-ignore
-    launchQueue.setConsumer(launchParams => {
-        handleFiles(launchParams.files);
-    });
 } else {
-    console.error('File Handling API is not supported!');
+
+    // Damit man nicht versehentlich das Browserfenster löscht und Eingaben verliert
+
+    window.addEventListener('beforeunload', function (event) {
+        event.preventDefault();
+        // Google Chrome requires returnValue to be set.
+        event.returnValue = '';
+        console.log("beforeunload", event)
+    });
+
+    if ('launchQueue' in window) {
+        console.log('File Handling API is supported!');
+
+        //@ts-ignore
+        launchQueue.setConsumer(launchParams => {
+            handleFiles(launchParams.files);
+        });
+    } else {
+        console.error('File Handling API is not supported!');
+    }
 }
 
-async function handleFiles(files:any) {
+async function handleFiles(files: any) {
     for (const file of files) {
         const blob = await file.getFile();
         blob.handle = file;
