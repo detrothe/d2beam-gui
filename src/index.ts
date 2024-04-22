20cffe8286802fa101a8c3c0498686b852149b98
detrothe <betonkanu@web.de>
2024-04-22 18:54:23 +0200

    22.4.2024

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,17 @@ import { write } from './pages/utility'
 const isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1;
 console.log("isAndroid =", isAndroid)
 
+let dbPromise: any;
+
 if (isAndroid) {
+
+
+
     window.addEventListener('load', function () {
         write('Android load')
         window.history.pushState({}, '')
+        const input = window.localStorage.getItem('current_input');
+        write('LOAD  current input = ' + input)
     })
 
     // window.addEventListener('popstate', function (event) {
@@ -35,17 +42,27 @@ if (isAndroid) {
         // Google Chrome requires returnValue to be set.
         event.returnValue = 'hallo';
         write("beforeunload " + event)
+
+        if (dbPromise) {
+            dbPromise.then(db => db.close());
+            dbPromise = null;
+        }
+        window.localStorage.setItem('current_input', 'i-n-p-u-t');
+
     });
 
     window.addEventListener('pageshow', (event) => {
         if (event.persisted) {
-          write('This page was restored from the bfcache.');
+            write('This page was restored from the bfcache.');
         } else {
-          write('This page was loaded normally.');
+            write('This page was loaded normally.');
+            openDB();
+            const input = window.localStorage.getItem('current_input');
+            write('PAGE SHOW  current input = ' + input)
         }
-      });
+    });
 
-      window.addEventListener('pagehide', (event) => {
+    window.addEventListener('pagehide', (event) => {
         if (event.persisted) {
             write('This page *might* be entering the bfcache.');
             window.alert('This page *might* be entering the bfcache.');
@@ -53,7 +70,7 @@ if (isAndroid) {
             write('This page will unload normally and be discarded.');
             window.alert('This page will unload normally and be discarded.');
         }
-      });
+    });
 
     // window.addEventListener('load', function () {
     //     write('Android load')
@@ -71,6 +88,9 @@ if (isAndroid) {
     //     }
     // })
 
+
+
+
 } else {
 
     // Damit man nicht versehentlich das Browserfenster löscht und Eingaben verliert
@@ -80,6 +100,15 @@ if (isAndroid) {
         // Google Chrome requires returnValue to be set.
         event.returnValue = '';
         console.log("beforeunload", event)
+
+
+        if (dbPromise) {
+            dbPromise.then(db => db.close());
+            dbPromise = null;
+        }
+
+        window.localStorage.setItem('current_input', 'input');
+
     });
 
     if ('launchQueue' in window) {
@@ -92,6 +121,25 @@ if (isAndroid) {
     } else {
         console.error('File Handling API is not supported!');
     }
+
+
+    window.addEventListener('pageshow', (event) => {
+        if (event.persisted) {
+            write('This page was restored from the bfcache.');
+        } else {
+            write('This page was loaded normally.');
+            openDB();
+            const input = window.localStorage.getItem('current_input');
+            write('current input = ' + input)
+
+        }
+    });
+
+    window.addEventListener('load', () => {
+        const input = window.localStorage.getItem('current_input');
+        write('LOAD  current input = ' + input)
+
+    });
 }
 
 async function handleFiles(files: any) {
@@ -102,4 +150,16 @@ async function handleFiles(files: any) {
 
         console.log(`${file.name} handled, content: ${text}`);
     }
+}
+
+function openDB() {
+    if (!dbPromise) {
+        dbPromise = new Promise((resolve, reject) => {
+            const req = indexedDB.open('my-db', 1);
+            req.onupgradeneeded = () => req.result.createObjectStore('keyval');
+            req.onerror = () => reject(req.error);
+            req.onsuccess = () => resolve(req.result);
+        });
+    }
+    return dbPromise;
 }
