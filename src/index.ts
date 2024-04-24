0c2931a386e340b914e43f7894973aae30be5b2b
detrothe <betonkanu@web.de>
2024-04-24 15:15:46 +0200

    24.4.2024

--- src/index.ts
+++ src/index.ts
@@ -7,103 +7,103 @@ import './pages/haupt';
 import { write } from './pages/utility'
 import { str_inputToJSON, read_daten } from './pages/dateien'
 
-// const isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1;
-// console.log("isAndroid =", isAndroid)
+const isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1;
+console.log("isAndroid =", isAndroid)
 
 // let dbPromise: any;
-// let input: any;
+let input: any;
 
-// if (isAndroid) {
-
-
-
-//     window.addEventListener('load', function () {
-//         write('Android load')
-//         //window.history.pushState({}, '')
-//         input = window.localStorage.getItem('current_input');
-//         write('LOAD  current input = ' + input)
-//         if (input.length > 0) read_daten(input);
-//     })
-
-//     // window.addEventListener('popstate', function (event) {
-//     //     write('Android popstate')
-//     //     window.history.pushState({}, '')
-//     //     event.preventDefault();
-//     // })
-
-//     document.addEventListener('visibilitychange', function () {
-//         write('Android visibilitychange = ' + document.visibilityState)
-//         // fires when user switches tabs, apps, goes to homescreen, etc.
-//         if (document.visibilityState === 'hidden') { write('Android hidden') }
-
-//         // fires when app transitions from prerender, user returns to the app / tab.
-//         if (document.visibilityState === 'visible') { write('Android visible') }
-//     });
-
-//     window.addEventListener('beforeunload', function (event) {
-//         //window.alert('beforeunload');
-//         event.preventDefault();
-//         // Google Chrome requires returnValue to be set.
-//         event.returnValue = '';
-//         //write("beforeunload " + event)
-
-//         // if (dbPromise) {
-//         //     dbPromise.then(db => db.close());
-//         //     dbPromise = null;
-//         // }
-//         window.localStorage.setItem('current_input', str_inputToJSON());
-
-//     });
-
-//     window.addEventListener('pageshow', (event) => {
-//         if (event.persisted) {
-//             write('This page was restored from the bfcache.');
-//         } else {
-//             write('This page was loaded normally.');
-//             // openDB();
-//             const input = window.localStorage.getItem('current_input');
-//             write('PAGE SHOW  current input = ' + input)
-//         }
-//     });
-
-//     window.addEventListener('pagehide', (event) => {
-//         if (event.persisted) {
-//             write('This page *might* be entering the bfcache.');
-//             //window.alert('This page *might* be entering the bfcache.');
-//         } else {
-//             write('This page will unload normally and be discarded.');
-//             //window.alert('This page will unload normally and be discarded.');
-//         }
-//     });
-
-//     // window.addEventListener('load', function () {
-//     //     write('Android load')
-//     //     window.history.pushState({ noBackExitsApp: true }, '')
-//     //     // window.history.back();
-//     //     // window.history.forward();
-//     // })
-
-//     // window.addEventListener('popstate', function (event) {
-//     //     write('Android popstate'+event.state)
-//     //     if (event.state && event.state.noBackExitsApp) {
-//     //         window.history.pushState({ noBackExitsApp: true }, '')
-//     //         // window.history.back();
-//     //         // window.history.forward();
-//     //     }
-//     // })
+if (isAndroid) {
 
 
 
+    //     window.addEventListener('load', function () {
+    //         write('Android load')
+    //         //window.history.pushState({}, '')
+    //         input = window.localStorage.getItem('current_input');
+    //         write('LOAD  current input = ' + input)
+    //         if (input.length > 0) read_daten(input);
+    //     })
 
-// } else {
+    //     // window.addEventListener('popstate', function (event) {
+    //     //     write('Android popstate')
+    //     //     window.history.pushState({}, '')
+    //     //     event.preventDefault();
+    //     // })
+
+        document.addEventListener('visibilitychange', function () {
+            write('Android visibilitychange = ' + document.visibilityState)
+            // fires when user switches tabs, apps, goes to homescreen, etc.
+            if (document.visibilityState === 'hidden') { write('Android hidden') }
+
+            // fires when app transitions from prerender, user returns to the app / tab.
+            if (document.visibilityState === 'visible') { write('Android visible') }
+        });
+
+    //     window.addEventListener('beforeunload', function (event) {
+    //         //window.alert('beforeunload');
+    //         event.preventDefault();
+    //         // Google Chrome requires returnValue to be set.
+    //         event.returnValue = '';
+    //         //write("beforeunload " + event)
+
+    //         // if (dbPromise) {
+    //         //     dbPromise.then(db => db.close());
+    //         //     dbPromise = null;
+    //         // }
+    //         window.localStorage.setItem('current_input', str_inputToJSON());
+
+    //     });
+
+    //     window.addEventListener('pageshow', (event) => {
+    //         if (event.persisted) {
+    //             write('This page was restored from the bfcache.');
+    //         } else {
+    //             write('This page was loaded normally.');
+    //             // openDB();
+    //             const input = window.localStorage.getItem('current_input');
+    //             write('PAGE SHOW  current input = ' + input)
+    //         }
+    //     });
+
+    //     window.addEventListener('pagehide', (event) => {
+    //         if (event.persisted) {
+    //             write('This page *might* be entering the bfcache.');
+    //             //window.alert('This page *might* be entering the bfcache.');
+    //         } else {
+    //             write('This page will unload normally and be discarded.');
+    //             //window.alert('This page will unload normally and be discarded.');
+    //         }
+    //     });
+
+    //     // window.addEventListener('load', function () {
+    //     //     write('Android load')
+    //     //     window.history.pushState({ noBackExitsApp: true }, '')
+    //     //     // window.history.back();
+    //     //     // window.history.forward();
+    //     // })
+
+    //     // window.addEventListener('popstate', function (event) {
+    //     //     write('Android popstate'+event.state)
+    //     //     if (event.state && event.state.noBackExitsApp) {
+    //     //         window.history.pushState({ noBackExitsApp: true }, '')
+    //     //         // window.history.back();
+    //     //         // window.history.forward();
+    //     //     }
+    //     // })
+
+
+
+
+} else {
 
     // Damit man nicht versehentlich das Browserfenster löscht und Eingaben verliert
 
-    // window.addEventListener('beforeunload', function (event) {
-    //     //event.preventDefault();
-    //     // Google Chrome requires returnValue to be set.
-    //     //event.returnValue = '';
-    //     console.log("beforeunload", event)
+    window.addEventListener('beforeunload', function (event) {
+        event.preventDefault();
+        // Google Chrome < 119 requires returnValue to be set.
+        event.returnValue = true;
+        console.log("beforeunload", event.target)
 
 
         // if (dbPromise) {
@@ -111,9 +111,9 @@ import { str_inputToJSON, read_daten } from './pages/dateien'
         //     dbPromise = null;
         // }
 
-        //window.localStorage.setItem('current_input', str_inputToJSON());
+        window.localStorage.setItem('current_input', str_inputToJSON());
 
-    // });
+    });
 
     // if ('launchQueue' in window) {
     //     console.log('File Handling API is supported!');
@@ -154,7 +154,7 @@ import { str_inputToJSON, read_daten } from './pages/dateien'
     //     }
     // });
 
-// }
+}
 
 async function handleFiles(files: any) {
     for (const file of files) {
