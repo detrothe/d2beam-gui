6968a11ec87812a38c77a65fad37b873fec25e96
detrothe <betonkanu@web.de>
2025-07-07 10:42:16 +0200

    7.7.2025

--- src/pages/haupt.ts
+++ src/pages/haupt.ts
@@ -74,7 +74,7 @@ import { set_max_lastfall, zero_max_lastfall } from "./cad_draw_elementlasten";
 import { reset_cad_nodes } from "./cad_node";
 
 //########################################################################################################################
-let theFooter = "2D structural analysis of frames and trusses, v1.4.1,a, 6-Juli-2025, ";
+let theFooter = "2D structural analysis of frames and trusses, v1.4.1, 7-Juli-2025, ";
 //########################################################################################################################
 
 let hostname = window.location.hostname;
