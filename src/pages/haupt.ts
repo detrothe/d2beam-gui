be22c867ac7cf0dc53a5b11b076fb758997e91e0
detrothe <betonkanu@web.de>
2025-11-13 21:59:44 +0100

    13.11.2025

--- src/pages/haupt.ts
+++ src/pages/haupt.ts
@@ -81,7 +81,7 @@ import { reset_cad_nodes } from './cad_node';
 import { info_Eigenwertberechnung, info_Materialeigenschaften } from './infos';
 
 //########################################################################################################################
-let theFooter = '2D structural analysis of frames and trusses, v1.7.0, 4-November-2025, ';
+let theFooter = '2D structural analysis of frames and trusses, v1.7.0, 13.November-2025, ';
 //########################################################################################################################
 
 let hostname = window.location.hostname;
