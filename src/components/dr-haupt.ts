7e175cb96cbeb24fb8cf29d4e0f03ac1d395c886
detrothe <betonkanu@web.de>
2026-03-12 21:23:50 +0100

    12.3.2026

--- src/components/dr-haupt.ts
+++ src/components/dr-haupt.ts
@@ -104,7 +104,7 @@ console.log('typs_string_kombitabelle', typs_string_kombitabelle);
 // stylesheet.replace(styles);
 
 //########################################################################################################################
-let theFooter = '2D structural analysis of frames and trusses, v1.8.2, 11.März-2026, ';
+let theFooter = '2D structural analysis of frames and trusses, v1.8.2, 12.März-2026, ';
 //########################################################################################################################
 
 // console.log("getBasePath()",getBasePath())
