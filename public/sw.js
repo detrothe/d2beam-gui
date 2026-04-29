c04f0fb2b7f5bc7237e4b58f6e408150b1ccd598
detrothe <betonkanu@web.de>
2026-04-29 16:22:45 +0200

    29.4.2026

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@ importScripts(
    'https://storage.googleapis.com/workbox-cdn/releases/7.4.0/workbox-sw.js'
 );
 
-const version = '1.8.12.0';
+const version = '1.8.13.0';
 console.log('Version', version);
 
 workbox.precaching.cleanupOutdatedCaches();
